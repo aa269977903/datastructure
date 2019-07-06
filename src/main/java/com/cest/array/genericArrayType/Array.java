9752f9151735d968653a498e221899cb0ac1c77f
cest <786638185@qq.com>
2019-07-06 20:38:30 +0800

    Initial commit

---
+++ src/main/java/com/cest/array/genericArrayType/Array.java
@@ -0,0 +1,154 @@
+package com.cest.array.genericArrayType;
+
+/**
+ * Created by Administrator on 2019/7/4.
+ */
+public class Array<E> {
+
+    private E[] data;
+    private int size;
+
+    // 构造函数,传入数组的容量capacity构造Array
+    public Array(int capacity){
+        data = (E[])new Object[capacity];
+        size = 0;
+    }
+
+    // 默认一个数量
+    public Array(){
+        this(0);
+    }
+
+    //  返回长度
+    public int getSize(){
+        return size;
+    }
+
+    //  返回长度
+    public int getCapactiy(){
+        return data.length;
+    }
+
+    //  返回数组为空
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    // 添加新元素
+    public void add(int index , E e){
+        if(size == data.length){
+            throw new IllegalArgumentException("Add failed Array is full");
+        }
+
+        if (index < 0 || index > size){
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+        }
+
+        for (int i = size-1; i >= index ; i--){
+            data[i+1] = data[i];
+        }
+
+        data[index] = e;
+
+        size++;
+    }
+
+    // 所有元素前面添加一个元素
+    public void addFirst(E e){
+        add(0,e);
+    }
+
+    //  所有元素之后添加一个元素
+    public void addLast(E e){
+        add(size,e);
+    }
+
+    //  获取index位置的元素
+    public E get(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+        }
+
+        return data[index];
+    }
+
+    //  修改索引处的元素
+    public void set(int index , E e){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+        }
+
+        data[index] = e;
+    }
+
+    //  查找数组之中是否有元素e
+    public boolean contains(E e){
+        for (int i = 0; i < size; i++) {
+            if(data[i].equals(e)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+    public int find(E e){
+        for (int i = 0; i < size; i++) {
+            if(data[i].equals(e)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 从数组中删除index位置的元素, 返回删除的元素
+    public E remove(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+        }
+
+        E ret = data[index];
+        for(int i = index ; i < size ; i++){
+            data[i] = data[i+1];
+        }
+
+        size--;
+        data[size] = null;
+        return ret;
+    }
+
+    // 从数组中删除第一个元素, 返回删除的元素
+    public E removeFirst(){
+        return remove(0);
+    }
+
+
+    // 从数组中删除最后一个元素, 返回删除的元素
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    // 从数组中删除元素e
+    public void removeElement(E e){
+        int i = find(e);
+        if(i != -1){
+            remove(i);
+        }
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+        res.append('[');
+        for(int i = 0 ; i < size ; i ++){
+            res.append(data[i]);
+            if(i != size - 1)
+                res.append(", ");
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+}
---
+++ src/main/java/com/leedcode/test1/leedcode1.java
@@ -0,0 +1,55 @@
+package com.leedcode.test1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * Created by Administrator on 2019/7/4.
+ */
+public class leedcode1 {
+
+    /**
+     * 穷举法 时间复杂度：O(n^2)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int i1 = i+1; i1 < nums.length; i1++) {
+                if(nums[i] + nums[i1] == target){
+                    return new int[]{i,i1};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    public static int[] twoSum2(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int i1 = target - nums[i];
+            if(map.containsKey(i1) && map.get(i1) != i){
+                return new int[]{i,map.get(i1)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] ints = twoSum2(new int[]{3, 2, 4}, 6);
+        System.out.println(ints);
+    }
+}
